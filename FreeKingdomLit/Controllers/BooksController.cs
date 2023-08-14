3d16f8ffadbb43bde4dd0e4772317b53f78e573a
Kiernan2 <kiernan1994@gmail.com>
2023-08-14 15:09:41 -0700

    reFactor books create with database

--- FreeKingdomLit/Controllers/BooksController.cs
+++ FreeKingdomLit/Controllers/BooksController.cs
@@ -1,67 +1,75 @@
 using Microsoft.AspNetCore.Mvc;
 using FreeKingdomLit.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FreeKingdomLit.Controllers
 {
   public class BooksController : Controller
   {
-    [HttpGet("/Books/")]
+
+    private readonly FreeKingdomLitContext _db;
+
+    public BooksController(FreeKingdomLitContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      List<Book> newlist = Book._instances;
+      List<Book> newlist = _db.Books.ToList();
       return View(newlist);
     }
 
-    [HttpGet("/Books/new")]
     public ActionResult Create()
     {
       return View();
     }
 
-    [HttpPost("/Books/Create")]
-    public ActionResult Create(string title, int pageNumber)
+    [HttpPost]
+    public ActionResult Create(Book book)
     {
-      Book book = new Book(title, pageNumber);
+      _db.Books.Add(book);
+      _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
-    [HttpGet("/Books/{id}")]
-    public ActionResult Details(int id)
-    {
-      Book book = Book.GetBook(id);
-      return View(book);
-    }
+    // [HttpGet("/Books/{id}")]
+    // public ActionResult Details(int id)
+    // {
+    //   Book book = Book.GetBook(id);
+    //   return View(book);
+    // }
 
-    [HttpGet("/Books/{id}/Edit")]
-    public ActionResult Edit(int id)
-    {
-      Book book = Book.GetBook(id);
-      return View(book);
-    }
+    // [HttpGet("/Books/{id}/Edit")]
+    // public ActionResult Edit(int id)
+    // {
+    //   Book book = Book.GetBook(id);
+    //   return View(book);
+    // }
 
-    [HttpPost("/Books/{id}/Edit")]
-    public ActionResult Edit(string title, int pageNumber , int id)
-    {
-      Book book = Book.GetBook(id);
-      book.Title = title;
-      book.Pages = pageNumber;
-      return RedirectToAction("Details", new {id});
-    }
+    // [HttpPost("/Books/{id}/Edit")]
+    // public ActionResult Edit(string title, int pageNumber , int id)
+    // {
+    //   Book book = Book.GetBook(id);
+    //   book.Title = title;
+    //   book.Pages = pageNumber;
+    //   return RedirectToAction("Details", new {id});
+    // }
 
-    [HttpGet("/Books/{id}/Delete")]
-    public ActionResult Delete(int id)
-    {
-      Book book = Book.GetBook(id);
-      return View(book);
-    }
+    // [HttpGet("/Books/{id}/Delete")]
+    // public ActionResult Delete(int id)
+    // {
+    //   Book book = Book.GetBook(id);
+    //   return View(book);
+    // }
 
-    [HttpPost("/Books/Delete"), ActionName("Delete")]
-    public ActionResult DeleteConfirmed (int id)
-    {
-      Book book = Book.GetBook(id);
-      Book._instances.Remove(book);
-      return RedirectToAction("Index");
-    }
+    // [HttpPost("/Books/Delete"), ActionName("Delete")]
+    // public ActionResult DeleteConfirmed (int id)
+    // {
+    //   Book book = Book.GetBook(id);
+    //   Book._instances.Remove(book);
+    //   return RedirectToAction("Index");
+    // }
   }
 }
--- FreeKingdomLit/Models/Book.cs
+++ FreeKingdomLit/Models/Book.cs
@@ -7,19 +7,5 @@ namespace FreeKingdomLit.Models
     public int BookId { get; set; }
     public string Title { get; set; }
     public int Pages { get; set; }
-    public static List<Book> _instances = new List<Book>();
-
-    public Book(string title,int pages)
-    {
-      _instances.Add(this);
-      BookId = _instances.Count;
-      Title = title;
-      Pages = pages;
-    }
-
-    public static Book GetBook(int id)
-    {
-      return _instances[id - 1];
-    }
   }
 }
