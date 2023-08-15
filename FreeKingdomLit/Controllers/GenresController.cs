0deb1322e0df746537127c159d4c4581ae399f77
Kiernan2 <kiernan1994@gmail.com>
2023-08-15 15:32:33 -0700

    add read functionality genre

--- FreeKingdomLit/Controllers/GenresController.cs
+++ FreeKingdomLit/Controllers/GenresController.cs
@@ -1,66 +1,74 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using FreeKingdomLit.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FreeKingdomLit.Controllers
 {
   public class GenresController : Controller
   {
-    [HttpGet("/Genres/")]
+
+    private readonly FreeKingdomLitContext _db;
+
+    public GenresController(FreeKingdomLitContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      List<Genre> genres = Genre._instances;
+      List<Genre> genres = _db.Genres.ToList();
       return View(genres);
     }
 
-    [HttpGet("/Genres/new")]
-    public ActionResult Create()
-    {
-      return View();
-    }
+    // public ActionResult Create()
+    // {
+    //   return View();
+    // }
 
-    [HttpPost("/Genres/Create")]
-    public ActionResult Create(string name)
-    {
-      Genre genre = new Genre(name);
-      return RedirectToAction("Index");
-    }
+    // [HttpPost]
+    // public ActionResult Create(string name)
+    // {
+    //   Genre genre = new Genre(name);
+    //   return RedirectToAction("Index");
+    // }
 
-    [HttpGet("/Genres/{id}")]
-    public ActionResult Details(int id)
-    {
-      Genre genre = Genre.GetGenre(id);
-      return View(genre);
-    }
+    // [HttpGet("/Genres/{id}")]
+    // public ActionResult Details(int id)
+    // {
+    //   Genre genre = Genre.GetGenre(id);
+    //   return View(genre);
+    // }
 
-    [HttpGet("/Genres/{id}/Edit")]
-    public ActionResult Edit(int id)
-    {
-      Genre genre = Genre.GetGenre(id);
-      return View(genre);
-    }
+    // [HttpGet("/Genres/{id}/Edit")]
+    // public ActionResult Edit(int id)
+    // {
+    //   Genre genre = Genre.GetGenre(id);
+    //   return View(genre);
+    // }
 
-    [HttpPost("/Genres/{id}/Edit")]
-    public ActionResult Edit(string name, int id)
-    {
-      Genre genre = Genre.GetGenre(id);
-      genre.Name = name;
-      return RedirectToAction("Details", new {id});
-    }
+    // [HttpPost("/Genres/{id}/Edit")]
+    // public ActionResult Edit(string name, int id)
+    // {
+    //   Genre genre = Genre.GetGenre(id);
+    //   genre.Name = name;
+    //   return RedirectToAction("Details", new {id});
+    // }
 
-    [HttpGet("/Genres/{id}/Delete")]
-    public ActionResult Delete(int id)
-    {
-      Genre genre = Genre.GetGenre(id);
-      return View(genre);
-    }
+    // [HttpGet("/Genres/{id}/Delete")]
+    // public ActionResult Delete(int id)
+    // {
+    //   Genre genre = Genre.GetGenre(id);
+    //   return View(genre);
+    // }
 
-    [HttpPost("/Genres/Delete"), ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      Genre genre = Genre.GetGenre(id);
-      Genre._instances.Remove(genre);
-      return RedirectToAction("Index");
-    }
+    // [HttpPost("/Genres/Delete"), ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   Genre genre = Genre.GetGenre(id);
+    //   Genre._instances.Remove(genre);
+    //   return RedirectToAction("Index");
+    // }
   }
 }
--- FreeKingdomLit/Models/Genre.cs
+++ FreeKingdomLit/Models/Genre.cs
@@ -6,18 +6,5 @@ namespace FreeKingdomLit.Models
   {
     public int GenreId { get; set; }
     public string Name { get; set; }
-    public static List<Genre> _instances = new List<Genre>();
-
-    public Genre(string name)
-    {
-      Name = name;
-      _instances.Add(this);
-      GenreId = _instances.Count;
-    }
-
-    public static Genre GetGenre(int id)
-    {
-      return _instances[id - 1];
-    }
   }
 }
