71c7314e20d5f977b0eb9d1c76b05a321c2aa8d8
Kiernan2 <kiernan1994@gmail.com>
2023-08-16 15:44:22 -0700

    Display genre and books on homepage

--- FreeKingdomLit/Controllers/HomeController.cs
+++ FreeKingdomLit/Controllers/HomeController.cs
@@ -1,18 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using FreeKingdomLit.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FreeKingdomLit.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FreeKingdomLitContext _db;
+
+    public HomeController(FreeKingdomLitContext db)
+    {
+      _db = db;
+    }
+
     [Route("/")]
     public ActionResult Index()
     {
-      // List<Genre> genreList = Genre._instances;
-      // ViewBag.GenreList = genreList;
-      // List<Book> bookList = Book._instances;
-      return View();
+      List<Genre> genreList = _db.Genres.ToList();
+      ViewBag.GenreList = genreList;
+      List<Book> bookList = _db.Books.ToList();
+      return View(bookList);
     }
   }
 }
