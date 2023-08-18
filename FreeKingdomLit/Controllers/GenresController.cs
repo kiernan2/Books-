fcf9f8c2cb70c86c971ebf2e299b372697a1b72f
Kiernan2 <kiernan1994@gmail.com>
2023-08-18 14:38:34 -0700

    Re-factor genres update

--- FreeKingdomLit/Controllers/GenresController.cs
+++ FreeKingdomLit/Controllers/GenresController.cs
@@ -54,12 +54,18 @@ namespace FreeKingdomLit.Controllers
     public ActionResult Edit(int id)
     {
       Genre thisGenre = _db.Genres.FirstOrDefault(genre => genre.GenreId == id);
+      ViewBag.BookId = new SelectList(_db.Books, "BookId", "Title");
       return View(thisGenre);
     }
 
     [HttpPost]
-    public ActionResult Edit(Genre genre)
+    public ActionResult Edit(Genre genre, int bookId)
     {
+      bool duplicate = _db.BooksGenres.Any(join => join.BookId == bookId && join.GenreId == genre.GenreId);
+      if(!duplicate && bookId != 0)
+      {
+        _db.BooksGenres.Add(new BookGenre() { GenreId = genre.GenreId, BookId = bookId});
+      }
       _db.Entry(genre).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Details", new { id = genre.GenreId });
